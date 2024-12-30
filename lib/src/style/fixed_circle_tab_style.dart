bca98df98e0855bd3423dffe6f9428b23b3619dc
YersultanSS <104438473+ErsultanSs@users.noreply.github.com>
2024-12-30 15:42:55 +0500

    Update fixed_circle_tab_style.dart

--- lib/src/style/fixed_circle_tab_style.dart
+++ lib/src/style/fixed_circle_tab_style.dart
@@ -29,7 +29,7 @@ class FixedCircleTabStyle extends InnerBuilder {
 
     // Explicitly define the text style with font size 9
     var textStyle = style.textStyle(c, item.fontFamily).copyWith(
-          fontSize: 12, // Set title size to 12
+          fontSize: 11, // Set title size to 12
         );
 
     var margin = style.activeIconMargin;
